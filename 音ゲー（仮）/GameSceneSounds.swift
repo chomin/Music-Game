24c1d5cdbad811f02de518cba00679ef2c69e16e
植田暢大 <uedanobuhiro@MacBook-Air.local>
2017-09-28 02:58:44 +0900

    レスポンスの改善(今度こそ！)
    mp3だと無音区間がうまく消えなかったのでタップ音をwavに変更

--- 音ゲー（仮）/GameSceneSounds.swift
+++ 音ゲー（仮）/GameSceneSounds.swift
@@ -13,29 +13,29 @@ import AVFoundation
 extension GameScene{
 	
 	func setAllSounds(){
-		BGM = setSound(fileName: bgmName)
-		tapSound1 = setSound(fileName: "タップ")
-		tapSound2 = setSound(fileName: "タップ")
-		tapSound3 = setSound(fileName: "タップ")
-		tapSound4 = setSound(fileName: "タップ")
-		flickSound1 = setSound(fileName: "フリック")
-		flickSound2 = setSound(fileName: "フリック")
-		flickSound3 = setSound(fileName: "フリック")
-		flickSound4 = setSound(fileName: "フリック")
+        BGM = setSound(fileName: bgmName, type: "mp3")
+        tapSound1 = setSound(fileName: "タップ", type: "wav")
+		tapSound2 = setSound(fileName: "タップ", type: "wav")
+		tapSound3 = setSound(fileName: "タップ", type: "wav")
+		tapSound4 = setSound(fileName: "タップ", type: "wav")
+		flickSound1 = setSound(fileName: "フリック", type: "wav")
+		flickSound2 = setSound(fileName: "フリック", type: "wav")
+		flickSound3 = setSound(fileName: "フリック", type: "wav")
+		flickSound4 = setSound(fileName: "フリック", type: "wav")
 
-		kara1 = setSound(fileName: "空打ち")
-		kara2 = setSound(fileName: "空打ち")
-		kara3 = setSound(fileName: "空打ち")
-		kara4 = setSound(fileName: "空打ち")
+		kara1 = setSound(fileName: "空打ち", type: "wav")
+		kara2 = setSound(fileName: "空打ち", type: "wav")
+		kara3 = setSound(fileName: "空打ち", type: "wav")
+		kara4 = setSound(fileName: "空打ち", type: "wav")
 		
 		BGM!.numberOfLoops = 0	//１度だけ再生
 	}
 	
-	func setSound(fileName:String) -> AVAudioPlayer!{//効果音を設定する関数
+    func setSound(fileName:String, type:String) -> AVAudioPlayer!{//効果音を設定する関数
 		var sound:AVAudioPlayer!
 		
 		// サウンドファイルのパスを生成
-		let Path = Bundle.main.path(forResource: fileName, ofType: "mp3")!    //m4a,oggは不可
+		let Path = Bundle.main.path(forResource: fileName, ofType: type)!    //m4a,oggは不可
 		let soundURL:URL = URL(fileURLWithPath: Path)
 		// AVAudioPlayerのインスタンスを作成
 		do {
