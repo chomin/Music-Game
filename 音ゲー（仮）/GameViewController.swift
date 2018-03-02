5e9272150270c76e03bee06a930cd3cddafad12d
nobu-g <ueda-n127@hotmail.co.jp>
2018-03-02 13:35:35 +0900

    GameViewController.swiftのインデントをスベースに変更

--- 音ゲー（仮）/GameViewController.swift
+++ 音ゲー（仮）/GameViewController.swift
@@ -13,26 +13,26 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-	
-	super.viewDidLoad()
-	let scene = ChooseMusicScene(size: view.bounds.size)
-	let skView = view as! SKView
-	skView.showsFPS = true
-	skView.showsNodeCount = true
-	skView.showsDrawCount = true
-	
-	skView.ignoresSiblingOrder = true
-	scene.scaleMode = .resizeFill
-	skView.presentScene(scene)  //ChooseMusicSceneに移動
-
+        
+        super.viewDidLoad()
+        let scene = ChooseMusicScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.showsDrawCount = true
+        
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)  // ChooseMusicSceneに移動
+        
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -40,12 +40,12 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
