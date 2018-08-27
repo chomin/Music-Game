b6f0ebe2dc52f7f26cd110a056f1aa8919ed7c75
Nakai <chomin_nakai.0120@icloud.com>
2018-08-27 22:49:24 +0900

    みっくとコラボしてたら完璧だった

--- 音ゲー（仮）/Dimensions.swift
+++ 音ゲー（仮）/Dimensions.swift
@@ -50,6 +50,7 @@ enum MusicName: String, EnumCollection {
     case teardrops              = "ティアドロップス(expert)"
     case kirakiraDatoka         = "キラキラだとか夢だとか~Sing Girls~(expert)"
     case godKnows               = "God knows…(expert)"
+    case shugabita              = "シュガーソングとビターステップ(expert)"
     
     
     
